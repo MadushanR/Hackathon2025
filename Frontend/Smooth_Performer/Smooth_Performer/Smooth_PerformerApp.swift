44e020c24516c618171b67ecdede1e1bc1e7d23d
Digaant Dogra <dogra057@gmail.com>
2025-01-18 12:21:12 -0500

    Commiting file

---
+++ Frontend/Smooth_Performer/Smooth_Performer/Smooth_PerformerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smooth_PerformerApp.swift
+//  Smooth_Performer
+//
+//  Created by Digaant Dogra on 2025-01-18.
+//
+
+import SwiftUI
+
+@main
+struct Smooth_PerformerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
