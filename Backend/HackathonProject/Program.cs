3fa7f3e95e85ea285c0df1eab5c5f760f2b3e400
madus <madushanrajendran@gmail.com>
2025-01-18 17:24:56 -0500

    Added Courses Controller

--- Backend/HackathonProject/Program.cs
+++ Backend/HackathonProject/Program.cs
@@ -6,11 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Configure the DbContext to use SQL Server
 builder.Services.AddDbContext<DataContext>(
     options => options.UseSqlServer(
-        builder.Configuration.GetConnectionString("MySqlConnection")
-    )
-);
+        builder.Configuration.GetConnectionString("SqlConnection"),
+        sqlOptions => sqlOptions.EnableRetryOnFailure()
+    ));
 
 var app = builder.Build();
 
@@ -18,7 +19,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
