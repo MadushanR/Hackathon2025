d67c472fb3865b7add17ab4994512217a884fca9
Digaant Dogra <dogra057@gmail.com>
2025-01-18 12:23:25 -0500

    Login, signup and Home created

---
+++ Frontend/Smooth_Performer/Smooth_Performer/Login.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  Smooth_Performer
+//
+//  Created by Digaant Dogra on 2025-01-18.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login()
+}
