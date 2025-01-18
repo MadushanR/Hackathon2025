06643ae755e2a39c527710c52fd9e57d75f4ae50
madus <madushanrajendran@gmail.com>
2025-01-18 17:24:56 -0500

    Added Courses Controller

--- Backend/HackathonProject/Models/DataContext.cs
+++ Backend/HackathonProject/Models/DataContext.cs
@@ -8,6 +8,16 @@ namespace HackathonProject.Models
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>()
+                .HasOne(c => c.Student) // Each course belongs to one student
+                .WithMany(s => s.Courses) // Each student can have many courses
+                .HasForeignKey(c => c.StudentId)
+                .OnDelete(DeleteBehavior.Cascade); // Optional: Cascade delete when a student is removed
+        }
+
+
         public DbSet<Student> Students { get; set; }
         public DbSet<Course> Courses { get; set; }
     }
--- Backend/HackathonProject/obj/Debug/net8.0/HackathonProject.AssemblyInfo.cs
+++ Backend/HackathonProject/obj/Debug/net8.0/HackathonProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HackathonProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44e020c24516c618171b67ecdede1e1bc1e7d23d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c84f10657f7128b43855dd1c2efd3629aa87d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HackathonProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HackathonProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
