af36d0e3efc878bdb33a3b25d567f724760416f7
arjunanxnd <anandarjun48@gmail.com>
2025-01-18 18:06:00 -0500

    Save local changes before merging Arjun_Backend

--- Backend/HackathonProject/obj/Debug/net8.0/HackathonProject.AssemblyInfo.cs
+++ Backend/HackathonProject/obj/Debug/net8.0/HackathonProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HackathonProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+206af233e7ba5bd9ddead0530be29bbb577053ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe8acb1410573da33b1b02b5266c94889f052748")]
 [assembly: System.Reflection.AssemblyProductAttribute("HackathonProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HackathonProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
