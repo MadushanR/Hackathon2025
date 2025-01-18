dcf298c352dc634f58fbfdf00d9b2d713f56eeb2
arjunanxnd <anandarjun48@gmail.com>
2025-01-18 13:52:57 -0500

    Created model classes and the database

--- Backend/HackathonProject/Program.cs
+++ Backend/HackathonProject/Program.cs
@@ -1,7 +1,16 @@
+using HackathonProject.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DataContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("MySqlConnection")
+    )
+);
 
 var app = builder.Build();
 
