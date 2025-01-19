95782e736a45674f1d7b022348b68657870339fa
Digaant Singh Dogra <128942073+DigaantDogra@users.noreply.github.com>
2025-01-19 08:29:45 -0500

    Retrive Course completed

---
+++ Frontend/Smooth_Performer/Smooth_Performer/View/AddCourse.swift
@@ -0,0 +1,18 @@
+//
+//  AddCourse.swift
+//  Smooth_Performer
+//
+//  Created by Digaant Dogra on 2025-01-19.
+//
+
+import SwiftUI
+
+struct AddCourse: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddCourse()
+}
